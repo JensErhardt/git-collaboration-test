8891c4d51d4c1d5744fd47bc02a8b156267fd84f
Garxoza <garxoza@gmail.com>
2018-06-22 15:48:45 +0200

    2nd commit

--- git-collaboration-test copy/routes/index.js
+++ git-collaboration-test copy/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router  = express.Router();
+const Garxoza=require('../views/student3');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/student3', function (req, res) {
+  res.render('student3');
+})
 module.exports = router;
